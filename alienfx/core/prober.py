16fafa6f58acc122eed5e20e2825554b5b44fe85
Stephen Harris <trackmastersteve@users.noreply.github.com>
2017-10-02 16:57:45 -0700

    Update prober.py
    Added M17xR4

--- alienfx/core/prober.py
+++ alienfx/core/prober.py
@@ -46,6 +46,7 @@ import alienfx.core.controller_m14xr3
 import alienfx.core.controller_m15x
 import alienfx.core.controller_m17x
 import alienfx.core.controller_m17xr3
+import alienfx.core.controller_m17xr4
 import alienfx.core.controller_m18xr2
 
 class AlienFXProber:
