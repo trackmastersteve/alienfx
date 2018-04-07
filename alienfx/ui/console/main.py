86567fc4f25694c818c87feb6162275ff8f60d0f
Stephen Harris <trackmastersteve@users.noreply.github.com>
2018-04-07 07:12:05 -0700

    Update main.py

--- alienfx/ui/console/main.py
+++ alienfx/ui/console/main.py
@@ -2,7 +2,7 @@
 # main.py
 #
 # Copyright (C) 2013-2014 Ashwin Menon <ashwin.menon@gmail.com>
-# Copyright (C) 2015-2017 Track Master Steve <trackmastersteve@gmail.com>
+# Copyright (C) 2015-2018 Track Master Steve <trackmastersteve@gmail.com>
 #
 # Alienfx is free software.
 #
