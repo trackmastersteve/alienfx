525410151425b3cbcc175813f8d8303609267582
Stephen Harris <trackmastersteve@users.noreply.github.com>
2018-08-02 16:01:29 -0700

    changed version number

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ entry_points = {
 
 setup(
     name = "alienfx",
-    version = "2.3.1",
+    version = "2.3.2",
     fullname = "AlienFX Configuration Utility",
     description = "AlienFX Configuration Utility",
     author = "Track Master Steve",
