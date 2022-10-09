a672ff0b6ac9b3fc650da30fa1d2d70dc9610dc0
kimmywacky <44282461+kimmywacky@users.noreply.github.com>
2022-10-08 19:02:24 -0700

    updated pyusb version

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     url = "https://github.com/trackmastersteve/alienfx",
     license = "GPLv3",
     
-    install_requires = ["pyusb>=1.1.1"],
+    install_requires = ["pyusb>=1.2.1"],
     data_files = data_files,
     entry_points = entry_points,
     packages = find_packages(),
