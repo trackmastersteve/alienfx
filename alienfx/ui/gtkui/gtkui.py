fa993e168502e0ea69a333bf01a30a849160dde6
Stephen Harris <trackmastersteve@users.noreply.github.com>
2023-12-16 14:02:38 -0800

    Updated gtk version

--- alienfx/ui/gtkui/gtkui.py
+++ alienfx/ui/gtkui/gtkui.py
@@ -34,7 +34,7 @@ import sys
 import threading
 
 import gi
-gi.require_version('Gtk', '3.0')
+gi.require_version('Gtk', '4.0')
 from gi.repository import GObject
 from gi.repository import Gtk
 from gi.repository import Gdk
